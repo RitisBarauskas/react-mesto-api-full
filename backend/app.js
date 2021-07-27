b66c30f18a2f553b642e6eba2a18e48e72490801
Ritis <perm300ru@yandex.ru>
2021-07-28 02:03:01 +0500

    add crash

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use(requestLogger);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post("/signin", validationSignIn, login);
 app.post("/signup", validationSignUp, createUser);
 
