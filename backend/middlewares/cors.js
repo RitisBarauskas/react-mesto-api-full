0386c6246610859861c81b8a67b00cceff47ed6c
Ritis <perm300ru@yandex.ru>
2021-07-28 01:48:14 +0500

    update cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -13,13 +13,13 @@ const defaultCors = (req, res, next) => {
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
   const requestHeaders = req.headers["access-control-request-headers"];
-  if (allowedCors.includes(origin)) {
+  // if (allowedCors.includes(origin)) {
     res.header("Access-Control-Allow-Origin", "*");
-  }
-  if (method === "OPTIONS") {
+  // }
+  // if (method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
     res.header("Access-Control-Allow-Headers", requestHeaders);
-  }
+  // }
   next();
 };
 
